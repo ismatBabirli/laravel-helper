fef45c7db9ffe6875216e92783b3cb06cda82a5e
Ismat Babirli <ismat.babirli@knexel.com>
2018-10-24 11:47:27 +0400

    first commit

---
+++ src/HelperServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ismat\Helper;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelperServiceProvider extends ServiceProvider
+{
+
+
+    public function boot(){
+
+//        $this->mergeConfigFrom(
+//            __DIR__.'/config/iAuth.php', 'iAuth'
+//        );
+//
+//
+//        $this->publishes([
+//            __DIR__.'/config/iAuth.php' => config_path('iAuth.php'),
+//        ]);
+        $this->loadRoutesFrom(__DIR__.'/Auth/web.php');
+
+    }
+
+
+    public function register()
+    {
+
+    }
+}
